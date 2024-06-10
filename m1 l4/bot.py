173c5be2ce6b9f0fe9d49992b52d6b5ef28bbd18
omerburakyalcin <oburakyalcin@gmail.com>
2024-06-10 21:05:58 +0300

    Add files via upload

---
+++ m1 l4/bot.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+from bot_token import token
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} olarak giriş yaptık')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Merhaba! Ben {bot.user}, bir Discord sohbet botuyum!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+
+@bot.command()
+async def how_are_you(ctx):
+    await ctx.send("Bots always feel good!")
+
+bot.run(token)
